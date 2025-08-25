d88598cb5d5c47ad1b0bc1efd87106bf1ed899a8
ylyma <amylingyuhui@gmail.com>
2025-08-25 15:53:23 +0200

    init

---
+++ LinalgLibraries/GridOc.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.InteropServices;
+
+namespace LinalgLibraries;
+
+public static class GridOc
+{
+    private const string LibraryName = "gridoc.dll";
+    
+    [DllImport(LibraryName, CallingConvention = CallingConvention.Cdecl)]
+    public static extern void gridoc_solve(
+        uint collocation_points,
+        uint left_boundary_points,
+        uint right_boundary_points,
+        double r_max,
+        double time_constant,
+        [Out] double[] solution_out,
+        out int solution_size_out
+    );
+}
---
+++ LinalgLibraries/GridOcLibrary.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LinalgLibraries;
+
+public static class GridOcLibrary
+    {
+        public static double[] Solve(
+            uint collocationPoints = 50,
+            uint leftBoundaryPoints = 1,
+            uint rightBoundaryPoints = 1,
+            double rMax = 0.0,
+            double timeConstant = 1.0)
+        {
+            // since max collocation points is 200 before numerical instability,
+            // 202 * 202 = 40840 and 50000 is a safe estimate for solution buffer size
+            double[] solutionBuffer = new double[50000];
+            int actualSize;
+        
+            try
+            {
+                GridOc.gridoc_solve(
+                    collocationPoints, leftBoundaryPoints, rightBoundaryPoints,
+                    rMax, timeConstant, solutionBuffer, out actualSize);
+            }
+            catch (DllNotFoundException)
+            {
+                throw new InvalidOperationException("Could not find C++ library");
+            }
+        
+            double[] solution = new double[actualSize];
+            Array.Copy(solutionBuffer, solution, actualSize);
+        
+            return solution;
+        }
+    }
